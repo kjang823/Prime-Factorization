2d3bd700000d6048c09abd44499336e1a7f2f587
Kevin Jang <tul56945@temple.edu>
2025-02-07 19:57:31 -0500

    Implemented map check and update.

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -16,31 +16,29 @@ int main(){
 
     //Create a map of primes up to the sqrt of the number.
     //Handle the prime of 2 case.
-    // if (prime_Map.empty()){
-        // prime_Map.insert(make_pair(2,1));
+    if (prime_Map.empty()){
+        prime_Map.insert(make_pair(2,1));
 
     //Handle primes greater than 2 case. Only odd numbers need be considered this point forward.
-    // } else {
+    } else {
         for (int i = 3; i*i <= user_Input; i = i + 2){
-
             //So long a i divides user_Input, add the prime to the prime factors map, continue to add 1 for each successful prime factorization.
             cout << "Trying out factors of " << i << endl;
-            //If I divied evenly into user_Input, create the pair and add into map. Otherwise, skip.
+            //If I divided evenly into user_Input, create the pair and add into map. Otherwise, skip.
             while (user_Input % i == 0){
                 //Check if the number has been added to the map. If not add, else incrase power count by 1.
                 it_check = prime_Map.find(i);
                 if(it_check == prime_Map.end()){
                     prime_Map.insert(make_pair(i,1));
                 } else {
-                    cout << it_check->first;
+                    it_check->second = (it_check->second)+1;
+                    cout << (it_check->second) << endl;
                 }
 
-                // prime_Map.insert(make_pair(i,1));
-                // user_Input = user_Input/i;
-                // cout << "User Input is " << user_Input << " after dividing by " << i <<endl;
+                user_Input = user_Input/i;
             }
         }
-    // }
+    }
 
     //Print out the map of prime factorizations.
     for (auto prime_Num : prime_Map){
