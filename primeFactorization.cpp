e618d9dc5b8e0b2ac25ff36d00501112b866814f
Kevin Jang <tul56945@temple.edu>
2025-02-07 23:52:37 -0500

    Edge case catch if number is itself a prime number.

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -29,16 +29,20 @@ int main(){
     while (user_Input % 2 == 0){
         user_Input = updateElement(prime_Map,2,user_Input);
     }
-    // } else {
-        //Handle primes greater than 2 case. Only odd numbers need be considered this point forward. Only up to the sqrt of the number.
-        for (int i = 3; i*i <= user_Input; i = i + 2){
-            //So long a i divides user_Input, add the prime to the prime factors map, continue to add 1 for each successful prime factorization.
-            while (user_Input % i == 0){
-                //Check if the number has been added to the map. If not add, else incrase power count by 1.
-                user_Input = updateElement(prime_Map,i,user_Input);
-            }
+
+    //Handle primes greater than 2 case. Only odd numbers need be considered this point forward. Only up to the sqrt of the number.
+    for (int i = 3; i*i <= user_Input; i = i + 2){
+        //So long a i divides user_Input, add the prime to the prime factors map, continue to add 1 for each successful prime factorization.
+        while (user_Input % i == 0){
+            //Check if the number has been added to the map. If not add, else incrase power count by 1.
+            user_Input = updateElement(prime_Map,i,user_Input);
         }
-    // }
+    }
+
+    //If the number remaining is not 1, then it must be a prime number. Else we have found all prime factorizations.
+    while (user_Input != 1){
+        user_Input = updateElement(prime_Map,user_Input,user_Input);
+    }
 
     //Print out the map of prime factorizations.
     for (auto prime_Num : prime_Map){
