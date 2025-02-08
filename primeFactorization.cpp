ebb853c1cd31f82230ced12c2b8dba2af1634165
Kevin Jang <tul56945@temple.edu>
2025-02-07 23:31:26 -0500

    create and implement updateElement function

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 using namespace std;
 
+int updateElement( map <int, int>& givenMap, int prime_Key, int given_Number ){
+    map <int, int>::iterator itr;
+    itr = givenMap.find(prime_Key);
+    if(itr == givenMap.end()){
+        givenMap.insert(make_pair(prime_Key,1));
+    } else {
+        itr->second = (itr->second)+1;   
+    }
+    return given_Number/prime_Key;
+ }
 
 int main(){
 
@@ -16,29 +26,23 @@ int main(){
 
     //Create a map of primes up to the sqrt of the number.
     //Handle the prime of 2 case.
-    if (prime_Map.empty()){
-        prime_Map.insert(make_pair(2,1));
-    } else {
+    while (user_Input % 2 == 0){
+        user_Input = updateElement(prime_Map,2,user_Input);
+    }
+    // } else {
         //Handle primes greater than 2 case. Only odd numbers need be considered this point forward. Only up to the sqrt of the number.
         for (int i = 3; i*i <= user_Input; i = i + 2){
             //So long a i divides user_Input, add the prime to the prime factors map, continue to add 1 for each successful prime factorization.
             while (user_Input % i == 0){
                 //Check if the number has been added to the map. If not add, else incrase power count by 1.
-                it_check = prime_Map.find(i);
-                if(it_check == prime_Map.end()){
-                    prime_Map.insert(make_pair(i,1));
-                } else {
-                    it_check->second = (it_check->second)+1;
-                }
-
-                user_Input = user_Input/i;
+                user_Input = updateElement(prime_Map,i,user_Input);
             }
         }
-    }
+    // }
 
     //Print out the map of prime factorizations.
     for (auto prime_Num : prime_Map){
-        cout << "The Prime factors of the number you entered are(prime/power): " << prime_Num.first << " " << prime_Num.second << endl;
+        cout << "The Prime factors of the number you entered are(prime/power): " << prime_Num.first << "^" << prime_Num.second << endl;
     }
 
     return 0;
