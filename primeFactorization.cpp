caf02fea70df2f5386e89272e0ae96096f4c4e92
Kevin Jang <tul56945@temple.edu>
2025-10-30 21:30:34 -0400

    Reverting to int for basic buildup

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -30,8 +30,8 @@ using namespace std;
 //     return 0;
 // }
 
-unsigned long long updateElement( map <unsigned long long, int>& given_Map, unsigned long long prime_Key, unsigned long long given_Number ){
-    map <unsigned long long, int>::iterator target_Prime;
+int updateElement( map <int, int>& given_Map, int prime_Key, int given_Number ){
+    map <int, int>::iterator target_Prime;
     target_Prime = given_Map.find(prime_Key);
     if(target_Prime == given_Map.end()){
         given_Map.insert(make_pair(prime_Key,1));
@@ -42,7 +42,7 @@ unsigned long long updateElement( map <unsigned long long, int>& given_Map, unsi
     return given_Number/prime_Key;
 }
 
-bool primeCheck(map <unsigned long long, int>& prime_Map, unsigned long long num2Check){
+bool primeCheck(map <int, int>& prime_Map, int num2Check){
     for (auto prime_Num : prime_Map){
         if (num2Check % (prime_Num.first) == 0){
             return false;
@@ -62,56 +62,59 @@ int main(){
 
     cout << "Please enter the number you would like to check is a prime..." << endl;
     cin >> user_Input;  
+
+    int user_Number = stoi(user_Input);
+
+    printf("The number is: %d\n", user_Number);
     
-    // map<unsigned long long, int> prime_Map;
-    // unsigned long long user_Input;
+    map<int, int> prime_Map;
 
-    // //Take user requested number to prime factorize.
-    // //TODO: Check to make sure user input is of correct type. 
-    // cout << "Please enter the number you would like to check is a prime..." << endl;
-    // cin >> user_Input;
-    // if (user_Input > ULLONG_MAX){
-    //     cout << "The number is too large." << endl;
-    //     exit(EXIT_FAILURE);
-    // }
 
-    // cout << "The number you entered is: " << user_Input << endl;
+    //Take user requested number to prime factorize.
+    //TODO: Check to make sure user input is of correct type. 
 
-    // //Start timer. 
-    // auto start = high_resolution_clock::now();
+    if (user_Number > INT_MAX){
+        cout << "The number is too large." << endl;
+        exit(EXIT_FAILURE);
+    }
 
-    // //Start work.
-    // while (primeCheck(prime_Map,2) && user_Input % 2 == 0){
-    //     user_Input = updateElement(prime_Map,2,user_Input);
-    // }
+    cout << "The number you entered is: " << user_Number << endl;
 
-    // //Handle primes greater than 2 case. Only odd numbers need be considered this point forward. Only up to the sqrt of the number.
-    // //TODO: Going through every odd number is too resource intensive. Lots of wasted cycles. Should check if the number is a prime number first. 
-    // for (unsigned long long i = 3; i*i <= user_Input; i = i + 2){
+    //Start timer. 
+    auto start = high_resolution_clock::now();
 
-    //     while (user_Input % i == 0){
-    //         user_Input = updateElement(prime_Map,i,user_Input);
-    //     }
-    // }
+    //Start work.
+    while (primeCheck(prime_Map,2) && user_Number % 2 == 0){
+        user_Number = updateElement(prime_Map,2,user_Number);
+    }
 
-    // //If the number remaining is not 1, then it must be a prime number. Else we have found all prime factorizations.
-    // while (user_Input != 1){
-    //     user_Input = updateElement(prime_Map,user_Input,user_Input);
-    // }
+    //Handle primes greater than 2 case. Only odd numbers need be considered this point forward. Only up to the sqrt of the number.
+    //TODO: Going through every odd number is too resource intensive. Lots of wasted cycles. Should check if the number is a prime number first. 
+    for (unsigned long long i = 3; i*i <= user_Number; i = i + 2){
 
-    // //End work. 
+        while (user_Number % i == 0){
+            user_Number = updateElement(prime_Map,i,user_Number);
+        }
+    }
 
-    // //End timer.
-    // auto end = high_resolution_clock::now();
+    //If the number remaining is not 1, then it must be a prime number. Else we have found all prime factorizations.
+    while (user_Number != 1){
+        user_Number = updateElement(prime_Map,user_Number,user_Number);
+    }
 
-    // //Print out the map of prime factorizations.
-    // cout << "The Prime factors are (prime^power): ";
-    // for (auto prime_Num : prime_Map){
-    //     cout << prime_Num.first << "^" << prime_Num.second << " ";
-    // }
-    // cout << endl;
+    //End work. 
 
-    // cout << "Calculating prime factorization took " << (duration_cast<milliseconds>(end-start)).count() << " milliseconds." << endl;
+    //End timer.
+    auto end = high_resolution_clock::now();
 
-    // return 0;
+    //Print out the map of prime factorizations.
+    cout << "The Prime factors are (prime^power): ";
+    for (auto prime_Num : prime_Map){
+        cout << prime_Num.first << "^" << prime_Num.second << " ";
+    }
+    cout << endl;
+
+    cout << "Calculating prime factorization took " << (duration_cast<milliseconds>(end-start)).count() << " milliseconds." << endl;
+
+    return 0;
 }
