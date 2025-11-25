e3724f4b10e240140cff3046b974c05b0771e26b
Kevin Jang <tul56945@temple.edu>
2025-11-25 09:49:17 -0500

    Try something new.

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -3,17 +3,16 @@
 #include <chrono>
 #include <vector>
 #include <cstdint>
-
-
-using namespace std::chrono;
-using namespace std;
+#include <algorithm>
 #include <bitset>
+#include <cerrno>
 
-unsigned long long updateElement( map <unsigned long long, unsigned long long>& given_Map, unsigned long long prime_Key, unsigned long long given_Number ){
-    map <unsigned long long, unsigned long long>::iterator target_Prime;
+
+int updateElement( std::map <int, int>& given_Map, int prime_Key, int given_Number ){
+    std::map <int, int>::iterator target_Prime;
     target_Prime = given_Map.find(prime_Key);
     if(target_Prime == given_Map.end()){
-        given_Map.insert(make_pair(prime_Key,1));
+        given_Map.insert(std::make_pair(prime_Key,1));
     } else {
         target_Prime->second = (target_Prime->second)+1;   
     }
@@ -21,7 +20,7 @@ unsigned long long updateElement( map <unsigned long long, unsigned long long>& 
     return given_Number/prime_Key;
 }
 
-bool primeCheck(map <unsigned long long, unsigned long long>& prime_Map, unsigned long long num2Check){
+bool primeCheck(std::map <int, int>& prime_Map, int num2Check){
     for (auto prime_Num : prime_Map){
         if (num2Check % (prime_Num.first) == 0){
             return false;
@@ -31,32 +30,25 @@ bool primeCheck(map <unsigned long long, unsigned long long>& prime_Map, unsigne
 }
 
 int main(){
-
+    
+    std::map<int, int> prime_Map;
     //Use LONG LONG MAX for testing. 
 
     //TODO: Set bits for any int, then implement prime factorization on that.
-    cout << "THIS PROGRAM IS UNDER CONSTRUCTION." << endl;
+    std::cout << "THIS PROGRAM IS UNDER CONSTRUCTION." << std::endl;
 
-    string user_Input;
-    cout << "Enter a positive integer to prime factorize: ";
-    cin >> user_Input;
-    
-    map<unsigned long long, unsigned long long> prime_Map;
-    unsigned long long user_Number = strtoull(user_Input.c_str(), nullptr, 10);;
-    if (errno == ERANGE){
-        cout << "The number you have entered is out of range. Please enter a smaller positive integer." << endl;
-        return 1;
-    }
+    int user_Number;
+    std::cout << "Enter a positive integer to prime factorize: ";
+    std::cin >> user_Number;    
 
-
-    //Take user requested number to prime factorize.
-    //TODO: Check to make sure user input is of correct type. 
-
-    cout << "The number you entered is: " << user_Number << endl;
-
-    //Start timer. 
-    auto start = high_resolution_clock::now();
-
+    /*
+    TODO: Multithreading Implementation
+        1. Thread the clock
+        2. Thread integers to check if they are a prime number.
+        3. Thread to make sure that the prime numbers are prime factors of the user entered number.
+    */
+    auto start = std::chrono::high_resolution_clock::now();
+    //Start timer.
     //Start work.
     while (primeCheck(prime_Map,2) && user_Number % 2 == 0){
         user_Number = updateElement(prime_Map,2,user_Number);
@@ -64,7 +56,7 @@ int main(){
 
     //Handle primes greater than 2 case. Only odd numbers need be considered this point forward. Only up to the sqrt of the number.
     //TODO: Going through every odd number is too resource intensive. Lots of wasted cycles. Should check if the number is a prime number first. 
-    for (unsigned long long i = 3; i*i <= user_Number; i = i + 2){
+    for (int i = 3; i*i <= user_Number; i = i + 2){
 
         while (user_Number % i == 0){
             user_Number = updateElement(prime_Map,i,user_Number);
@@ -79,16 +71,16 @@ int main(){
     //End work. 
 
     //End timer.
-    auto end = high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
 
     //Print out the map of prime factorizations.
-    cout << "The Prime factors are (prime^power): ";
+    std::cout << "The Prime factors are (prime^power): ";
     for (auto prime_Num : prime_Map){
-        cout << prime_Num.first << "^" << prime_Num.second << " ";
+        std::cout << prime_Num.first << "^" << prime_Num.second << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << "Calculating prime factorization took " << (duration_cast<nanoseconds>(end-start)).count() << " nanoseconds." << endl;
+    std::cout << "Calculating prime factorization took " << (std::chrono::duration_cast<std::chrono::nanoseconds>(end-start)).count() << " nanoseconds." << std::endl;
 
     return 0;
 }
